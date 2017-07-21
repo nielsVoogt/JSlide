b60ac6833de040fc643ef4664898547315c6cae6
nielsVoogt <niels@branchonline.nl>
2017-07-21 16:21:03 +0200

    Cleaned up my gulpfile, added babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,41 @@
+
 'use strict';
 
-var gulp           = require('gulp'),
-    source         = require('vinyl-source-stream'),
-    browserify     = require('browserify'),
-    browserifycss  = require('browserify-css'),
-    buffer         = require('vinyl-buffer'),
-    es             = require('event-stream'),
-    notify         = require('gulp-notify'),
-    watch          = require('gulp-watch'),
-    browserSync    = require('browser-sync').create(),
-    watchify       = require('watchify'),
-    sass           = require('gulp-sass'),
-    sourcemaps     = require('gulp-sourcemaps'),
-    path           = require('path'),
-    autoprefixer   = require('gulp-autoprefixer'),
-    merge          = require('merge-stream');
+var gulp         = require('gulp'),
+    notify       = require('gulp-notify'),
+    watch        = require('gulp-watch'),
+    browserSync  = require('browser-sync').create(),
+    sass         = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    merge        = require('merge-stream'),
+    babel        = require('gulp-babel');
 
 function onError(err) {
     console.log(err);
     this.emit('end');
 }
 
-// --------------------------------------------------- SASS/CSS DEST
-
 var sassInput = './**/*.scss',
     devInput  = ['./**/*.html', './**/*.js'];
-
-// --------------------------------------------------- COMPILE OPTIONS
 
 var sassOptions = {
     errLogToConsole: true,
     outputStyle: 'expanded'
 };
 
-// --------------------------------------------------- AUTOPREFIXER OPTIONS
-
 var autoprefixerOptions = {
     browsers: ['last 2 versions', '> 1%', 'Safari 8', 'Firefox ESR']
 };
 
-// --------------------------------------------------- BROWSERSYNC TASK (combines 'sass' with 'browsersync')
-
 gulp.task('serve', ['sass'], function() {
-
     browserSync.init({
         proxy: "jslide.local.dev",
-        browser: ["google chrome"],
+        browser: ["google chrome"]
     });
-
     gulp.watch(sassInput, ['sass']);
     gulp.watch(sassInput).on('change', browserSync.reload);
     gulp.watch(devInput).on('change', browserSync.reload);
 });
-
-// --------------------------------------------------- SASS TASK
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
@@ -61,3 +43,11 @@ gulp.task('sass', function () {
     .pipe(autoprefixer(autoprefixerOptions))
     .pipe(gulp.dest('./example/css/'));
 });
+
+gulp.task('babel', () =>
+    gulp.src('jslide.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest('dist/'))
+);
